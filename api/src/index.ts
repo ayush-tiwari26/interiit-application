0f18cc6b8a8073793c6e89f515963bb0ec09be0b
Ayush Tiwari <ayushtiwari26402@gmail.com>
2022-12-16 03:46:59 +0530

    add db connection

---
+++ api/src/index.ts
@@ -0,0 +1,14 @@
+import {app} from './app';
+import {AppDataSource} from '../omrconfig';
+import "reflect-metadata";
+
+main();
+
+async function main() {
+    if(process.env.JWT_SECRET_KEY === undefined && process.env.NODE_ENV!=="production") {
+        process.env.JWT_SECRET_KEY = "JWT_SECRET";
+    }
+    await AppDataSource.initialize();
+    console.log('Postgres connection established');
+    app.listen(process.env.PORT, () => console.log('Server is running on Port'+process.env.PORT));
+}
